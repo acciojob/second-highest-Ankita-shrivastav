f64a59cc002539ec56604e796168d67b3a133163
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2024-08-25 00:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your JS code here. If required.
+function secondHighest(arr){
+	if(arr.length<=1||new Set(arr).size===1){
+		return -Infinity;
+	}
+	let max=Math.max(...arr);
+	let secondMax=-Infinity;
 
+	for(let num of arr){
+		if(num<max && num>secondMax){
+			secondMax=num;
+		}
+	}
+	return secondMax;
+}
